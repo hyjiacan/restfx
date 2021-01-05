a1a1da114a6860a7173b7d32a946b250244c84e3
hyjiacan <hyjiacan@163.com>
2021-01-05 09:08:54 +0800

    Revert 中间件调用

--- src/restfx/middleware/interface.py
+++ src/restfx/middleware/interface.py
@@ -10,13 +10,11 @@ class MiddlewareBase(ABC):
     def __int__(self, *args, **kwargs):
         pass
 
-    @classmethod
-    def dispose(cls):
+    def dispose(self):
         pass
 
-    @classmethod
     @abstractmethod
-    def process_request(cls, request, meta, **kwargs):
+    def process_request(self, request, meta, **kwargs):
         """
         对 request 对象进行预处理。一般用于请求的数据的解码，此时路由组件尚水进行请求数据的解析(B,P,G 尚不可用)
         :param request:
@@ -27,9 +25,8 @@ class MiddlewareBase(ABC):
         """
         pass
 
-    @classmethod
     @abstractmethod
-    def process_invoke(cls, request, meta, **kwargs):
+    def process_invoke(self, request, meta, **kwargs):
         """
         在路由函数调用前，对其参数等进行处理，此时路由组件已经完成了请求数据的解析(B,P,G 已可用)
         此时可以对解析后的参数进行变更
@@ -41,9 +38,8 @@ class MiddlewareBase(ABC):
         """
         pass
 
-    @classmethod
     @abstractmethod
-    def process_return(cls, request, meta, data, **kwargs):
+    def process_return(self, request, meta, data, **kwargs):
         """
         在路由函数调用后，对其返回值进行处理
         :param request:
@@ -56,9 +52,8 @@ class MiddlewareBase(ABC):
         """
         pass
 
-    @classmethod
     @abstractmethod
-    def process_response(cls, request, meta, response, **kwargs):
+    def process_response(self, request, meta, response, **kwargs):
         """
         对 response 数据进行预处理。一般用于响应的数据的编码
         :param request:
