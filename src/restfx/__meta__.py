af48cf229c8b71711684a10fabed9a5a79d0cd2b
hyjiacan <hyjiacan@163.com>
2021-03-09 11:15:26 +0800

    build 0.9.0

--- src/restfx/__meta__.py
+++ src/restfx/__meta__.py
@@ -1,3 +1,3 @@
 name = 'restfx'
-version = '0.8.0'
+version = '0.9.0'
 website = 'https://gitee.com/hyjiacan/restfx'
--- src/restfx/context.py
+++ src/restfx/context.py
@@ -1,5 +1,4 @@
 import os
-from types import FunctionType
 
 from restfx import __meta__
 from .routes.collector import Collector
