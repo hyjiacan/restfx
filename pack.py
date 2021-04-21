093afd5f02bcd94f190698dc28d9e81937890f38
hyjiacan <hyjiacan@163.com>
2021-04-21 15:22:40 +0800

    优化 build 处理文件

--- pack.py
+++ pack.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import time
 
 from tarfile import TarFile
 
@@ -34,7 +36,7 @@ def tar_dir(archive_path: str, dir_path: str):
         return False, e.__str__()
 
 
-if __name__ == '__main__':
+def package_sample():
     print('Packaging sample')
     root = os.path.dirname(__file__)
     sampledir = os.path.abspath(os.path.join(root, 'src/restfx/internal_assets/sample'))
@@ -42,7 +44,67 @@ if __name__ == '__main__':
 
     try:
         tar_dir(tarname, sampledir)
-        print('Complete!\n')
+        print('Package complete!\n')
+        time.sleep(1)
     except Exception as e:
         print('Failure:' + str(e))
         exit(1)
+
+
+def renew_version():
+    # 用法
+    # build m 更新主版本号
+    # build s 更新次版本号
+    # build b 更新 build 版本号 (默认值)
+    # build x 不更新版本号
+    command = 'b' if len(sys.argv) == 1 else sys.argv[1]
+    if command == 'x':
+        return
+
+    if command not in ('m', 's', 'b'):
+        print('Unknown version command %r' % command)
+        exit(1)
+        return
+
+    print('Renewing version with command %r...' % command)
+
+    root = os.path.dirname(__file__)
+    meta_file = os.path.abspath(os.path.join(root, 'src/restfx/__meta__.py'))
+
+    try:
+        with open(meta_file, mode='r', encoding='utf8') as fp:
+            lines = fp.readlines()
+        # 版本在第二行，可以写死
+        version_line = lines[1].strip()
+        version = version_line.split('=')[1].strip(' \'')
+        print('The old version is %s' % version)
+        [major, second, build] = version.split('.')
+
+        if command == 'm':
+            major = int(major) + 1
+            second = 0
+            build = 0
+        elif command == 's':
+            second = int(second) + 1
+            build = 0
+        else:
+            build = int(build) + 1
+
+        new_version = '%s.%s.%s' % (major, second, build)
+
+        print('The new version is %r' % new_version)
+
+        lines[1] = 'version = \'%s\'\n' % new_version
+
+        with open(meta_file, mode='w', encoding='utf8') as fp:
+            fp.writelines(lines)
+        print('Renew version complete!\n')
+        time.sleep(1)
+    except Exception as e:
+        print('Failure:' + str(e))
+        exit(1)
+
+
+if __name__ == '__main__':
+    package_sample()
+    renew_version()
--- src/restfx/__meta__.py
+++ src/restfx/__meta__.py
@@ -1,3 +1,3 @@
 name = 'restfx'
-version = '0.14.1'
+version = '0.14.2'
 website = 'https://gitee.com/hyjiacan/restfx'
